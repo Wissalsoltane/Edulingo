d3cf1a762db4e93ca635b00354811188c4cdae6c
Wissalsoltane <164432613+Wissalsoltane@users.noreply.github.com>
2024-05-28 01:54:07 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,23 +1,36 @@
 <?php
+//session
 session_start();
+
 include 'Connection.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  $sql = "SELECT * FROM user WHERE username='".$username."' AND password='".$password."';";
+  $sql = "SELECT * FROM user WHERE username = '$username'";
   $result = mysqli_query($conn, $sql);
 
   if (mysqli_num_rows($result) <= 0) {
-      $_SESSION['login_error'] = "Invalid username or password.";
-      header("Location: login.html");
-      exit;
+    //cookie
+    setcookie('error', true, time() + 3600, '/');
+    header("Location: login.html");
+    exit;
   }
 
   $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-  $_SESSION['user'] = $row['id'];
+
+  if ($password != $row['password']) {
+    setcookie('error', true, time() + 3600, '/');
+    header("Location: login.html");
+    exit;
+  }
+  
+
+  $_SESSION['user'] = $row;
   header("Location: level.html");
-  exit;
 }
+
 ?>
+
+
